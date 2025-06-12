0cfc79551d09f45b6f6aff22fb97aabda58ebcb9
Davy Kyrian <davy.kyrian@gmx.de>
2025-06-13 00:02:03 +0200

    delete card fix and gitignore fix

--- card/delete_card.php
+++ card/delete_card.php
@@ -9,11 +9,22 @@ if (!$userId || !$id) die("Zugriff verweigert.");
 $stmt = $pdo->prepare("SELECT image_text, image_qr FROM cards WHERE id = ? AND user_id = ?");
 $stmt->execute([$id, $userId]);
 $card = $stmt->fetch();
+
 if ($card) {
-    unlink("card/images/" . $card['image_text']);
-    unlink("card/images/" . $card['image_qr']);
+    $imageBasePath = realpath(__DIR__ . '/../card/images/');
+    $imageTextPath = $imageBasePath . '/' . $card['image_text'];
+    $imageQrPath = $imageBasePath . '/' . $card['image_qr'];
+
+    if (is_file($imageTextPath)) {
+        if (!unlink($imageTextPath)) error_log("Fehler beim Löschen von $imageTextPath");
+    }
+    if (is_file($imageQrPath)) {
+        if (!unlink($imageQrPath)) error_log("Fehler beim Löschen von $imageQrPath");
+    }
+
     $del = $pdo->prepare("DELETE FROM cards WHERE id = ? AND user_id = ?");
     $del->execute([$id, $userId]);
 }
+
 header("Location: ../profile.php");
 exit;
